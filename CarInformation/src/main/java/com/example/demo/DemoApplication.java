a1434c80d7939e1d215f4390386ad7f2cbad51ce
saiabirami kalidoss <saiabiramikalidoss@gmail.com>
2023-11-09 00:39:10 +0530

    Initial commit

--- CarInformation/src/main/java/com/example/demo/DemoApplication.java
+++ CarInformation/src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		System.out.println("Car information");
+		System.out.println("Car information updated");
 	}
 
 }
