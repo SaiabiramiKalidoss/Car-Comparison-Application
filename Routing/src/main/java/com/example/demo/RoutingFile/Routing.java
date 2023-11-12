b2d0e1cda68721905acbbb670a586efce2fd3c55
saiabirami kalidoss <saiabiramikalidoss@gmail.com>
2023-11-12 22:41:15 +0530

    Authentication changes

--- Routing/src/main/java/com/example/demo/RoutingFile/Routing.java
+++ Routing/src/main/java/com/example/demo/RoutingFile/Routing.java
@@ -4,21 +4,29 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 public class Routing {
+
+    @Value("${APIKEY}")
+    private String apiKey;
 
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
             .route("LoginService", r -> r.path("/login/**")
-                .uri("http://localhost:4001")) 
-            .route("FilterCriteriaService", r -> r.path("/registration/**")
-                .uri("http://localhost:4002")) 
+                .filters(f -> f.addRequestHeader("X-API-KEY", "apiKey"))
+                .uri("http://localhost:4001"))
+            .route("RegistrationService", r -> r.path("/registration/**")
+                .filters(f -> f.addRequestHeader("X-API-KEY", "apiKey"))
+                .uri("http://localhost:4002"))
             .route("FilterCriteriaService", r -> r.path("/filtercriteria/**")
-                .uri("http://localhost:4003")) 
-            .route("FilterCriteriaService", r -> r.path("/carinformation/**")
-                .uri("http://localhost:4004")) 
+                .filters(f -> f.addRequestHeader("X-API-KEY", "apiKey"))
+                .uri("http://localhost:4003"))
+            .route("CarInformationService", r -> r.path("/carinformation/**")
+                .filters(f -> f.addRequestHeader("X-API-KEY", "apiKey"))
+                .uri("http://localhost:4004"))
             .build();
     }
 }
